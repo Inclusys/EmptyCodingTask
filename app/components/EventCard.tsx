302b0d310e050e7617cafce63e2eba1ecd90d980
shaafay <shaafay@start2fix.com>
2026-01-29 16:47:20 +0100

    Add responsive icon and circle.

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 type Props = {
@@ -6,25 +7,61 @@ type Props = {
 };
 
 export function EventCard({ title, icon }: Props) {
-  return (
-    <View style={styles.card}>
-      <Text style={styles.icon}>{icon}</Text>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-}
+    const [circleSize, setCircleSize] = useState(0);
+    const circleSizeFactor = 0.4
+
+    const handleCircleLayout = (event) => {
+        setCircleSize(event.nativeEvent.layout.width);
+    };
+
+    const getIconSize = () => {
+        return circleSize * circleSizeFactor;
+    };
+
+    return (
+        <View style={styles.card}>
+            <View style={styles.iconContainer}>
+                <View style={styles.iconCircle} onLayout={handleCircleLayout}>
+                    <Text style={[styles.icon, { fontSize: getIconSize() }]}>{icon}</Text>
+                </View>
+            </View>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        </View>
+    );
+    }
 
 const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    backgroundColor: "red",
-    borderRadius: 8,
-    padding: 6,
-  },
-  icon: {
-    fontSize: 32,
-  },
-  title: {
-    marginTop: 4,
-  },
+    card: {
+        flex: 1,
+        backgroundColor: "red",
+        borderRadius: 8,
+        padding: 6,
+    },
+    icon: {
+    },
+    title: {
+        marginTop: 4,
+    },
+    iconContainer: {
+        flex: 2,
+        justifyContent: "center", // centers icon vertically in its space
+        alignItems: "center", // centers icon horizontally
+    },
+    titleContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    iconCircle: {
+        width: "70%",
+        maxHeight: "100%",
+        maxWidth: "100%",
+        aspectRatio: 1,
+        borderRadius: 9999, 
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
